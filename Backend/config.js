c42a41384cb7e63c432b14548074618575760161
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-04-16 23:44:10 +0530

    first commit

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  // "mongodb+srv://khajjayamsathvik:sathvik@cluster0.wjidugt.mongodb.net/";
+  "mongodb+srv://ritesh17rb:fh72tRwSCcC2O4jJ@cluster0.fc1sxpg.mongodb.net/bookStore";
